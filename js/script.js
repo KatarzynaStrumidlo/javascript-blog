d99262c900b3c9165739aedb572e952bcf6278b1
Katarzyna Strumidło <strumidlokd@gmail.com>
2021-01-14 14:03:12 +0100

    Add class 'active' to the correct article

--- js/script.js
+++ js/script.js
@@ -30,7 +30,8 @@ const titleClickHandler = function(event){
     /* [DONE] find the correct article using the selector (value of 'href' attribute) */
     const targetArticle = document.querySelector(articleSelector);
     console.log(targetArticle);
-    /* [IN PROGRESS] add class 'active' to the correct article */
+    /* [DONE] add class 'active' to the correct article */
+    targetArticle.classList.add('active');
   }
 
   const links = document.querySelectorAll('.titles a');
