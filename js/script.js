96109f8f150f81121afc92ae732593140e91d7eb
Katarzyna Strumidło <strumidlokd@gmail.com>
2021-01-15 16:06:43 +0100

    Generate title list

--- js/script.js
+++ js/script.js
@@ -2,40 +2,58 @@
 const titleClickHandler = function(event){
     event.preventDefault();
     const clickedElement = this;
-    console.log('Link was clicked!');
-    console.log(event);
-
     /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
-
     for(let activeLink of activeLinks){
-    activeLink.classList.remove('active');
+      activeLink.classList.remove('active');
     }
-
     /* [DONE] add class 'active' to the clicked link */
-
-  console.log('clickedElement:', clickedElement);
-  clickedElement.classList.add('active');
-
-    /* [DONE] remove class 'active' from all articles */
+    clickedElement.classList.add('active');
+  /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts .post.active');
-
     for(let activeArticle of activeArticles){
-    activeArticle.classList.remove('active');
+      activeArticle.classList.remove('active');
     }
     /* [DONE] get 'href' attribute from the clicked link */
     const articleSelector = clickedElement.getAttribute('href');
-    console.log(articleSelector);
-
     /* [DONE] find the correct article using the selector (value of 'href' attribute) */
     const targetArticle = document.querySelector(articleSelector);
-    console.log(targetArticle);
     /* [DONE] add class 'active' to the correct article */
     targetArticle.classList.add('active');
   }
 
-  const links = document.querySelectorAll('.titles a');
 
-  for(let link of links){
-    link.addEventListener('click', titleClickHandler);
+const optArticleSelector = '.post',
+optTitleSelector = '.post-title',
+optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* [DONE] remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+  /* [DONE]for each article */
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  let html = '';
+  for(let article of articles){
+    /* [DONE] get the article id */
+    const articleId = article.getAttribute('id');
+    /*[DONE] find the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    /* [DONE]get the title from the title element */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    /*[DONE] create HTML of the link */
+    titleList.insertAdjacentHTML('beforeend', linkHTML);
+    /*[DONE] insert link into titleList */
+    html = html + linkHTML;//nie romumiem tutaj o co chodzi, bo działa tak samo jak jest ten fragment i jak go nie ma
   }
+  titleList.innerHTML = html;
+  const links = document.querySelectorAll('.titles a');
+  for(let link of links){
+      link.addEventListener('click', titleClickHandler);
+    }
+
+}
+
+generateTitleLinks();
