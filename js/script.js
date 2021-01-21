fd2e4a1309b2d9146b8eb4194925b72208e4d983
Katarzyna Strumidło <strumidlokd@gmail.com>
2021-01-21 14:24:31 +0100

    Error

--- js/script.js
+++ js/script.js
@@ -63,16 +63,55 @@ function generateTags(){
     /* START LOOP: for each tag */
     for(let tag of articleTagsArray){
       /* generate HTML of the link */
-      const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>'+ '  ';
-      /* add generated code to html variable */
+      const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>'+ ' ';
+      /* insert HTML of all the links into the tags wrapper  */
       tagList.insertAdjacentHTML('beforeend', linkHTML);
     /* END LOOP: for each tag */
     }
-    /* insert HTML of all the links into the tags wrapper */
-
   /* END LOOP: for every article: */
+  }
+}
+generateTags();
+function tagClickHandler(event){
+  /* prevent default action for this event */
+  debugger;
+  //event.preventDefault();
+  /* make new constant named "clickedElement" and give it the value of "this" */
+  const clickedElement = this;
+  /* make a new constant "href" and read the attribute "href" of the clicked element */
+  const href = clickedElement.getAttribute('href');
+  console.log(href);
+  /* make a new constant "tag" and extract tag from the "href" constant */
+  const tag = href.replace('#tag-', '');
+  console.log(tag);
+  /* find all tag links with class active */
+  const tagLinks = document.querySelectorAll('a.active[href^="#tag-"]');
+  console.log(tagLinks);
+  /* START LOOP: for each active tag link */
+  for (let tagLink of tagLinks){
+    /* remove class active */
+    tagLink.classList.remove('active');
+  /* END LOOP: for each active tag link */
+  }
+  /* find all tag links with "href" attribute equal to the "href" constant */
 
- }
+  /* START LOOP: for each found tag link */
+
+    /* add class active */
+
+  /* END LOOP: for each found tag link */
+
+  /* execute function "generateTitleLinks" with article selector as argument */
+}
+tagClickHandler();
+function addClickListenersToTags(){
+  /* find all links to tags */
+
+  /* START LOOP: for each link */
+
+    /* add tagClickHandler as event listener for that link */
+
+  /* END LOOP: for each link */
 }
 
-generateTags();
+addClickListenersToTags();
